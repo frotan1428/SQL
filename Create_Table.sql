0f1c377c25b7df2ddd7170c7cce602fe931a4cd0
MFROTAN <98325864+frotan1428@users.noreply.github.com>
2022-04-09 11:19:10 +0430

    How_to_create_table

---
+++ Create_Table.sql
@@ -0,0 +1,12 @@
+
+-- How to create students tableinside the techproed Data Base
+CREATE TABLE students(
+   std_id INT NOT NULL,
+   std_name VARCHAR(25),
+   std_dob DATE,
+   std_phone CHAR(10),
+   std_email VARCHAR(30),
+   PRIMARY KEY (std_email)
+);
+
+
